2fc7b76b8dfdf16a201fb8fc3bdf576f4d18bd72
Mayank Jaat <153971576+Mayankjat005@users.noreply.github.com>
2024-08-21 11:04:55 +0530

    Update script.py

--- Youtube/script.py
+++ Youtube/script.py
@@ -4,12 +4,12 @@ class Translation(object):
 
     ABOUT_TXT = """
 ╭───────────⍟
-├📛 **My Name** : [YouTube Video Uploader Bot](https://t.me/YouTubeUploaderOneBot)
+├📛 **My Name** : [YouTube Video dOWNLOADER Bot](https://t.me/YouTubeVideoDownloader0Bot)
 ├📢 **Framework** : [Pyrogram 2.0.106](https://docs.pyrogram.org/)
 ├💮 **Language** : [Python 3.12.3](https://www.python.org)
-├👥 **Support Group** : [NT BOTS SUPPORT](https://t.me/NT_BOTS_SUPPORT)
-├🥏 **Channel** : [NT BOT CHANNEL](https://t.me/NT_BOT_CHANNEL)
-├⛲ **Source** : [Click Here](https://github.com/LISA-KOREA/YouTube-Video-Download-Bot)
-├🎓 **Developer** : [LISA 👑](https://t.me/LISA_FAN_LK)
+├👥 **Support Group** : [WARRIOR BOTS SUPPORT](https://t.me/+XAtFZIm4GNZmNGNl)
+├🥏 **Channel** : [Warrior Units Bots](https://t.me/WarriorUnitsBots)
+├⛲ **Anime channel** : [Warrior Units](https://t.me/Warrior_Units)
+├🎓 **Developer** : [Stranger ](https://t.me/oo7jatji)
 ╰───────────────⍟
 """
